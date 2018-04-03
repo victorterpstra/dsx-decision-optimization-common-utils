398ba5f8aef5aa6f7f95fd856129a0c91337aebf
victorterpstra <38053845+victorterpstra@users.noreply.github.com>
2018-04-03 17:59:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='dsx_decision_optimization_common_utils',
       version='0.1',
       description='Common optimization utilities for use in Python-based optmization projects in IBM DSX',
-      url='https://github.ibm.com/DSE/dsx-decision-optimization-common_utils',
+      url='https://github.ibm.com/DSE/dsx_decision_optimization_common_utils',
       author='Victor Terpstra',
       author_email='vterpstra@us.ibm.com',
       license='MIT',
